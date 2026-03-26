aaca56d51a7fb0b253f588ac20b16bcb8de50e4b
Oskar Jauch <oskar.jauch@posteo.de>
2026-03-26 23:31:09 +0100

    Include crawls with empty wacz archives

--- crawler/management/commands/finish_crawls.py
+++ crawler/management/commands/finish_crawls.py
@@ -1,13 +1,23 @@
+import logging
+
 from django.core.management.base import BaseCommand
 from django.db.models import Q
 
 from crawler.crawl_runner import finish_crawl
 from crawler.models import Crawl
 
+logger = logging.getLogger(__name__)
+
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        for crawl in Crawl.objects.filter(
-            Q(status="running") | Q(status="finished", wacz_archive__isnull=True)
-        ):
+        crawls = Crawl.objects.filter(
+            Q(status="running")
+            | (
+                Q(status="finished")
+                & (Q(wacz_archive__isnull=True) | Q(wacz_archive=""))
+            )
+        )
+        print(f"Found {crawls.count()} crawls")
+        for crawl in crawls:
             finish_crawl(crawl)
